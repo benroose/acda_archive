3973b88175d48754977689a43af0c88caa569592
greyarea <greyarea@MARTELL.fire.ice>
2017-03-04 22:26:13 -0600

    upload of streaming script

---
+++ streaming_scripts/stream_screen.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Script to stream to http server
+# NOTE: Needs echo, vlc installed
+
+# variables and inputs
+
+dest_ip="";
+net_port="18080";
+#stream_path="/stream.webm"
+stream_path="/stream.wmv";
+
+case "${1}" in
+    screen)
+	input_device="screen:// :screen-fps=10 :screen-caching=100"
+        ;;
+    video)
+	input_device="v4l2:///dev/video0:fps=10"
+	;;
+    *)
+	echo "Specify screen or video"
+	exit 1
+	;;
+esac
+
+# SCRIPT
+
+# check input file exists (exit if not)
+#if [ ! -f ${input_file} ]; then
+#    /bin/echo "${input_file} not found!";
+#    exit 1;
+
+/bin/echo "Attempting to stream from ${input_device}";
+
+# http webserver version
+#cvlc ${input_device} --no-sout-standard-sap --sout-keep --ttl=20 --sout="#transcode{vcodec=theo,vb=2048,width=1280,height=720,fps=30,channels=1,samplerate=22050,acodec=acc}:std{access=http{mime=video/ogg},mux=ogg,dst=${dest_ip}:${net_port}${stream_path}}"
+
+# vlc client version
+cvlc ${input_device} --sout "#transcode{vcodec=wmv2,vb=256,scale=1,fps=10}:duplicate{dst=std{access=http{mime=video/x-ms-wmv},mux=asf,dst=${dest_ip}:${net_port}${stream_path}}}";
+#echo "cvlc ${input_device} --sout '#transcode{vcodec=wmv2,vb=256,scale=1,fps=25}:duplicate{dst=std{access=http{mime=video/x-ms-wmv},mux=asf,dst=${dest_ip}:${net_port}${stream_path}}}'"
