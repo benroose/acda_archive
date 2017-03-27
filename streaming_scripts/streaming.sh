4d623c62f89794bfbf93d2dd7c3c71a816eca7cb
Ben <ben.roose@wichita.edu>
2017-03-26 22:12:53 -0500

    update streaming script

--- streaming_scripts/streaming.sh
+++ streaming_scripts/streaming.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# TESTING Script to video stream to local http server and use python's inbuilt webserver
+# Script for video streaming to local http server using python's inbuilt webserver
 # NOTE: Needs echo, python, and vlc installed
 
 # variables and inputs
@@ -8,7 +8,8 @@
 dest_ip=""
 vlc_screen_port="18080"
 vlc_video_port="18081"
-http_port="8000"
+#http_port="8000"
+#http_port="80"
 
 script_path="$HOME/acda_2017/streaming_scripts"
 tmp_file="/tmp/webstream.tmp"
@@ -16,8 +17,9 @@ tmp_file="/tmp/webstream.tmp"
 screen_path="/screen.webm"
 video_path="/video.webm"
 
-screen_input="screen:// :screen-fps=20 :screen-caching=100"
-video_input="v4l2:// /dev/video0:fps=20"
+screen_input="screen:// :screen-fps=10 :screen-caching=5"
+#screen_input="screen:// :screen-fps=10 :screen-caching=100"
+video_input="v4l2:// /dev/video0:fps=10"
 
 webserver_log="httpserver.log"
 vlc_screen_log="vlc_screen.log"
@@ -36,6 +38,7 @@ start_webserver() {
 /bin/echo "Attempting to start web server in background [log located in ${script_path}]";
 cd ${script_path};
 python -m SimpleHTTPServer ${http_port} &> ${webserver_log} &
+#sudo python -m SimpleHTTPServer ${http_port} &> ${webserver_log} &
 
 }
 
@@ -43,7 +46,9 @@ start_vlcstreams() {
 /bin/echo "Attempting to start vlc with screen input for http in background [log located in ${script_path}]";
 
 # webm transcode in vp80 THIS WORKS
-cvlc ${screen_input} --no-sout-standard-sap --sout-keep --ttl=20 --sout="#transcode{vcodec=VP80,vb=1024}:std{access=http{mime=video/webm},mux=webm,dst=${dest_ip}:${vlc_screen_port}${screen_path}}" > ${vlc_screen_log} &
+cvlc ${screen_input} --no-sout-standard-sap --sout-keep --ttl=20 --sout="#transcode{vcodec=VP80,vb=800}:std{access=http{mime=video/webm},mux=webm,dst=${dest_ip}:${vlc_screen_port}${screen_path}}" > ${vlc_screen_log} &
+
+#cvlc ${screen_input} --no-sout-standard-sap --sout-keep --ttl=20 --sout="#transcode{vcodec=VP80,vb=1024}:std{access=http{mime=video/webm},mux=webm,dst=${dest_ip}:${vlc_screen_port}${screen_path}}" > ${vlc_screen_log} &
 
 # webm transcode in vp80 resolution changes THIS WORKS
 #cvlc -v ${screen_input} --no-sout-standard-sap --sout-keep --ttl=20 --sout="#transcode{vcodec=VP80,vb=2048,width=1280,height=720,channels=1,samplerate=44100}:std{access=http{mime=video/webm},mux=webm,dst=${dest_ip}:${vlc_screen_port}${screen_path}}" > ${screen_log} &
@@ -55,7 +60,7 @@ cvlc ${screen_input} --no-sout-standard-sap --sout-keep --ttl=20 --sout="#transc
 #cvlc ${screen_input} --no-sout-standard-sap --sout-keep --ttl=20 --sout="#transcode{vcodec=theo,vb=1024,width=1024,height=720,fps=10,channels=1,samplerate=22050,acodec=acc}:std{access=http{mime=video/ogg},mux=ogg,dst=${dest_ip}:${vlc_screen_port}${screen_path}}" > ${vlc_screen_log} &
 
 /bin/echo "Attempting to start vlc with video input for http in background [log located in ${script_path}]";
-cvlc ${video_input} --no-sout-standard-sap --sout-keep --ttl=20 --sout="#transcode{vcodec=VP80,vb=1024,}:std{access=http{mime=video/webm},mux=webm,dst=${dest_ip}:${vlc_video_port}${video_path}}" > ${vlc_video_log} &
+cvlc ${video_input} --no-sout-standard-sap --sout-keep --ttl=20 --sout="#transcode{vcodec=VP80,vb=800,}:std{access=http{mime=video/webm},mux=webm,dst=${dest_ip}:${vlc_video_port}${video_path}}" > ${vlc_video_log} &
 
 #cvlc -v ${video_input} --no-sout-standard-sap --sout-keep --ttl=20 --sout="#transcode{vcodec=VP80,vb=2048,width=1280,height=720,channels=1,samplerate=44100}:std{access=http{mime=video/webm},mux=webm,dst=${dest_ip}:${vlc_video_port}${video_path}}"
 
--- streaming_scripts/test_stream_screen.sh
+++ streaming_scripts/test_stream_screen.sh
@@ -11,13 +11,15 @@ http_port="8000";
 stream_path="/stream.webm";
 #stream_path="/stream.wmv";
 
-input_screen="screen:// :screen-fps=20 :screen-caching=100"
+#input_screen="screen:// :screen-fps=20 :screen-caching=100 :screen-width=1280 :screen-height=800"
+input_screen="-vv screen:// :screen-fps=20 :screen-width=1280 :screen-height=800 :screen-top=0"
 input_video="v4l2:// /dev/video0:fps=20"
 
 case "${1}" in
     screen)
 	input_device="screen:// :screen-fps=20 :screen-caching=100"
-        ;;
+	#input_device="-vv screen:// :screen-fps=20 :screen-width=1280 :screen-height=800 :screen-top=0"
+    ;;
     video1)
 	input_device="v4l2:// /dev/video0:fps=10"
 	;;
@@ -45,8 +47,12 @@ esac
 
 /bin/echo "Attempting to stream from ${input_device}";
 
+#ffmpeg -f x11grab -s 320x240 -r 15 -i :0.0 http://localhost:8090/feed1.ffm
+
+#ffmpeg -f x11grab -s 1280x800 -framerate 15 -i :0.0 -c:v libx264 -preset fast -pix_fmt yuv420p -s 1280x800 -threads 0 -f mp4 "rtmp://live.twitch.tv/app/live_********_*****************************"
+
 # http webserver version no audio
-cvlc ${input_device} --no-sout-standard-sap --sout-keep --ttl=20 --sout="#transcode{vcodec=theo,vb=2048,width=1280,height=720,fps=20}:std{access=http{mime=video/ogg},mux=ogg,dst=${dest_ip}:${net_port}${stream_path}}"
+cvlc ${input_device} --no-sout-standard-sap --sout-keep --ttl=20 --sout="#transcode{vcodec=theo,vb=2048,width=1280,height=800,fps=20}:std{access=http{mime=video/ogg},mux=ogg,dst=${dest_ip}:${net_port}${stream_path}}"
 
 # http webserver version with audio
 #cvlc ${input_device} --no-sout-standard-sap --sout-keep --ttl=20 --sout="#transcode{vcodec=theo,vb=2048,width=1280,height=720,fps=20,channels=1,samplerate=22050,acodec=acc}:std{access=http{mime=video/ogg},mux=ogg,dst=${dest_ip}:${net_port}${stream_path}}"
